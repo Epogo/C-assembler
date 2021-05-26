23a6c186d5bd2a050a818a7e2bd6f09b07452eb4
dmuskal <dmuskal97@yahoo.com>
2021-05-26 01:25:30 -0400

    handleInput updated
    linked list implemented to store text

--- handleInput.c
+++ handleInput.c
@@ -2,14 +2,13 @@
 
 void handleFileContents(FILE *fd){
 
-	int charFromFile; /*declare variable for receiving characters from file*/
-	int lineCounter;
-	int newLineFlag = 1;
+	int charFromFile,lineCounter,index;
 	NODE_T *ptrNode; /*initialize pointer to NODE_T, variable ptr_node*/
 	NODE_T *head; /*initialize pointer to head of type NODE_T*/
 	NODE_T *current; /*initialize pointer to current node of type NODE_T*/
 	NODE_T *tmpPtr; /*initialize pointer to tmpPtr of type NODE_T*/
 	lineCounter = 1;
+	index = 0;
 
 	tmpPtr = (NODE_T*)calloc(1, sizeof(NODE_T));
 	if(!tmpPtr)
@@ -26,32 +25,35 @@ void handleFileContents(FILE *fd){
 		charFromFile = fgetc(fd); /*store character from file in charFromFile variable*/
 		if(!feof(fd)){
 			/*printf("%c", charFromFile);*/ /*if the current character from the file is not EOF, then print the character*/
-
 			if(charFromFile != '\n'){
-				ptrNode->inputChar[index] = 
+				ptrNode->inputChar[index] = charFromFile;
+				index++;
 			}
-			
-			if(newLineFlag == 1){
+			else{
 				ptrNode->lineNumber = lineCounter;
+				lineCounter++;
 				current = ptrNode; /*sets the "current" node equal to ptrNode, pointing to the element whose buffer was just filled*/
 				tmpPtr = (NODE_T*)calloc(1, sizeof(NODE_T)); /*uses temporary pointer in case of error to dynamically allocate memory for an additional element to add to the linked list*/
 				if(!tmpPtr)
 				{
-					/*if no more memory can be allocated, set the error flag to ERROR and break out of the while loop*/
-					errorFlag = ERROR;
-					break;
+					exit(0);
 				}
 				ptrNode = tmpPtr; /*return the temporary pointer to the original pointer variable pointing to the new element after memory successfully allocated*/
 				current->next = ptrNode; /*sets pointer to the next element of the "current" node equal to ptrNode, whose buffer will be filled next*/
+				index = 0;
 			}
-
-		
-
 		}
 		else{
-			break; /*if the current character from the file is EOF, break from the while loop*/
+			/*ptrNode->next = NULL;*/
+			break;
 		}
-		ptrNode = head; /*set ptrNode equal to head to point to the first element of the list*/
+
 	}
+	ptrNode = head; /*set ptrNode equal to head to point to the first element of the list*/
+		
+
+
+
+
 	
 }
