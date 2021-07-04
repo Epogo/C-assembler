75174ad88e8f89a5ca33a016462d75863a63188f
Epogo <62990985+Epogo@users.noreply.github.com>
2021-07-04 13:38:33 +0300

    Update AsmToBin_2.c

--- AsmToBin_2.c
+++ AsmToBin_2.c
@@ -4,9 +4,10 @@
 #define NUMBIT 16
 void firstPass(char*,char*);
 char *Registers(char*);
+char *decToBin(char*);
 int main()
 {
-    firstPass("add","$4,$5,$6");
+    firstPass("addi","$4,15,$6");
     return 0;
 }
 
@@ -18,6 +19,8 @@ void firstPass(char *ptrField1,char *ptrField2){
     const char s[2] = ",";
     char *token;
     char *reg;
+    int count=0;
+    char *imm;
     token = strtok(str, s);
     
         if(!strcmp(ptrField1,"add")){
@@ -26,12 +29,30 @@ void firstPass(char *ptrField1,char *ptrField2){
             printf( "%s ", reg);
             free(reg);
             token = strtok(NULL, s);
+            }
         }
-    }
+        else if(!strcmp(ptrField1,"addi")){
+            while(token!=NULL){
+                if (count!=1)
+                    reg=Registers(token);
+                printf( "%s ", reg);
+                if (count==1){
+                    imm=decToBin(token);
+                }
+                if (count!=1)
+                    free(reg);
+                token = strtok(NULL, s);
+                count++;
+            }
+        }
+    
+    printf( "%s ", imm);
+    free(imm);
     free(str);
+    count=0;
+
 
 }
-
 char *Registers(char *reg)
 {
     int num;
@@ -40,7 +61,8 @@ char *Registers(char *reg)
     char *str=(char*)malloc(6);
     temp=reg;
     if (reg[0]!='$'){
-    	return;	
+        printf("Not a register!");
+    	exit(EXIT_FAILURE);	
     }
     temp++;
     num=atoi(temp);
@@ -58,4 +80,22 @@ char *Registers(char *reg)
                 i++;		
 	   }
     return str;
+}
+
+char *decToBin(char *number)
+{
+    int num;
+    int i,j;
+    char *str=(char*)malloc(17);
+    num=atoi(number);
+    str[16]='\0';
+    j=15;
+    while (j>=0)
+	   {
+		str[j--]=num%2+'0';
+                num=num/2;
+                i++;		
+	   }
+    return str;
+
 }
