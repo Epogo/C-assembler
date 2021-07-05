4e856ba64173e714b1eac4f13c91fad3cb25aff6
Epogo <62990985+Epogo@users.noreply.github.com>
2021-07-05 17:24:15 +0300

    Update AsmToBin_2.c

--- AsmToBin_2.c
+++ AsmToBin_2.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #define NUMBIT 16
 #define RCOMLEN 8
+#define ICOMLEN 5
 
 void firstPass(char*,char*,char*);
 char *Registers(char*);
@@ -14,11 +15,9 @@ char *rComFunct[]={"00001","00010","00011","00100","00101"};
 char *rOpCode[]={"000000","000001"};
 char *iOpCode[]={"01010","01011","01100","01101","01110"};
 
-char *rOpCode[]={"000000","000001"};
 int main()
 {
-    
-    firstPass(NULL,"move","$20,$2");
+    firstPass(NULL,"addi","$1,1,$2");
     return 0;
 }
 
@@ -31,11 +30,10 @@ void firstPass(char *ptrField1,char *ptrField2,char *ptrField3){
     char *token;
     char *reg;
     int count=0;
-    char *imm;
+    char *imm=(char *)calloc(17, sizeof(char));;
     char *registers[3];
     
     token = strtok(str, s);
-    
     for(int i=0;i<RCOMLEN;i++){
         if(!strcmp(ptrField2,rCommands[i])){
             while( token != NULL ) {
@@ -77,6 +75,41 @@ void firstPass(char *ptrField1,char *ptrField2,char *ptrField3){
             printf("%s"," 000000");
         }
     }
+
+    for(int i=0;i<ICOMLEN;i++){
+
+        if(!strcmp(ptrField2,iCommands[i])){
+            while( token != NULL ) {
+                    if (count==1)
+                    {
+                        registers[count]=NULL;
+                        strcpy(imm,decToBin(token));
+                        count++;
+                        token = strtok(NULL, s);
+                        continue;
+                    }
+                    reg=Registers(token);
+                    registers[count]=(char*) malloc(6 * sizeof(char));
+                    strcpy(registers[count],reg);
+                    free(reg);
+                    token = strtok(NULL, s);
+                    count++;
+            }
+                
+            printf("%s",iOpCode[(i+10)%10]);
+            for (int j=0;j<3;j++)
+                {
+                    if (j==1)
+                        continue;
+                    printf(" %s",registers[j]);
+                    free(registers[j]);
+                }
+            printf(" %s",imm);
+            free(imm);
+                
+        }
+    }
+    
     free(str);
     count=0;
 }
