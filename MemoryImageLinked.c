1007dd864b32f5ce8b55e7453dd668bb4c8560bd
Epogo <62990985+Epogo@users.noreply.github.com>
2021-07-07 16:39:32 +0300

    Update MemoryImageLinked.c

--- MemoryImageLinked.c
+++ MemoryImageLinked.c
@@ -4,17 +4,23 @@
 
 typedef struct memoryImage{
     int address;
-    char op[5];
+    char op[32];
     struct memoryImage *next;
 } memIm;
 
-char *str[]={"0000","0001","0010","0011","0100"};
+char binToHex(char*);
+
+char *str[]={"00000110111101110101111111111110","11100110111101110101111111111011","00001110111101110101111111111011","00001110111101110101111111111011","00000111111101110101111111111010"};
 int main()
 {
     int add=100;
     memIm *head;
     memIm *current;
     memIm *temp;
+    char *bin;
+    char mem[5];
+    int j=0;
+    char hex;
     
     head=(struct memoryImage*) malloc(sizeof(memIm));
     current=head;
@@ -31,7 +37,22 @@ int main()
     temp=head;
     while (1){
         printf("%d ",head->address);
-        printf("%s",head->op);
+        printf("%s ",head->op);
+        bin=head->op;
+        for(int i=0;i<32;i++){
+            mem[j]=*bin;
+            if((i+1)%4==0){
+                mem[4]='\0';
+                //printf("%s ",mem);
+                hex=binToHex(mem);
+                j=0;
+                printf("%c ",hex);
+                bin++;
+                continue;
+            }
+            j++;
+            bin++;
+        }
         printf("\n");
         head=head->next;
         free(temp);
@@ -41,4 +62,42 @@ int main()
     }
 
     return 0;
+}
+
+char binToHex(char *bin)
+{
+    char hex;
+    if(!strcmp(bin, "0000"))
+        hex='0';
+    else if(!strcmp(bin, "0001"))
+        hex='1';
+    else if(!strcmp(bin, "0010"))
+        hex='2';
+    else if(!strcmp(bin, "0011"))
+        hex='3';
+    else if(!strcmp(bin, "0100"))
+        hex='4';
+    else if(!strcmp(bin, "0101"))
+        hex='5';
+    else if(!strcmp(bin, "0110"))
+        hex='6';
+    else if(!strcmp(bin, "0111"))
+        hex='7';
+    else if(!strcmp(bin, "1000"))
+        hex='8';
+    else if(!strcmp(bin, "1001"))
+        hex='9';
+    else if(!strcmp(bin, "1010"))
+        hex='A';
+    else if(!strcmp(bin, "1011"))
+        hex='B';
+    else if(!strcmp(bin, "1100"))
+        hex='C';
+    else if(!strcmp(bin, "1101"))
+        hex='D';
+    else if(!strcmp(bin, "1110"))
+        hex='E';
+    else if(!strcmp(bin, "1111"))
+        hex='F';
+    return hex;
 }
