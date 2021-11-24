6daf28c3f65a57e23809d74e9483010be603ef41
Epogo <62990985+Epogo@users.noreply.github.com>
2021-11-25 01:24:01 +0200

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -63,7 +63,7 @@ int main(int argc, char** argv){
 	if(argc == SINGLEARG){
 		/*if no files are added as arguments in the command line, then print out message to user of not enough inputs and exit program*/
 		printf("Not enough inputs \n");
-		exit(0);
+		exit(0);/*exit*/
 	}
 	else{
 		/*if arguments were added from the command line, enter readFile function with argument counter and argument vector as the function arguments*/
